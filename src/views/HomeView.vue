7c3cc0359f610199af4fadd03c1fd37f457c105f
Dark Mode <darkkmodee00@gmail.com>
2023-10-20 11:56:57 -0400

    bug: attempt at fixing import error

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
 <template>
   <main>
     <div class="full-center-container">
-      <img src="src/assets/pictures/panther.png" style="width: 100%">
+      <img src="/src/assets/pictures/panther.png" style="width: 100%">
       <h1 class="home-title">DARK MODE</h1>
     </div>
   </main>
