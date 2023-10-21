19016fba7d1771fea9e5e64a28b46d137d039676
Dark Mode <darkkmodee00@gmail.com>
2023-10-21 17:26:29 -0400

    feature: new links

--- src/components/headnav.vue
+++ src/components/headnav.vue
@@ -4,6 +4,8 @@
   <main>
     <nav class="headnav">
       <RouterLink class="headnav-link" to="/">Home</RouterLink>
+      <RouterLink class="headnav-link" to="/projects">Projects</RouterLink>
+      <RouterLink class="headnav-link" to="/projects">Store</RouterLink>
       <RouterLink class="headnav-link" to="/about">About</RouterLink>
     </nav>
   </main>
