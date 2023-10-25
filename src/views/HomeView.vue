d10c41a6e5548502d727a2b23838b72b206ecbca
Dark Mode <darkkmodee00@gmail.com>
2023-10-25 14:07:50 -0400

    fixed path

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import FeaturedProjects from "@/components/FeaturedProjects.vue"
+let pathToImage = new URL('/src/assets/panther_home.png', import.meta.url).href
 </script>
 
 <template>
   <v-app>
-    <v-img src="/src/assets/panther_home.png"></v-img>
+    <v-img :src="pathToImage"></v-img>
     <img src="/src/assets/panther_home.png" style="width: 100%">
     <FeaturedProjects></FeaturedProjects>
   </v-app>
