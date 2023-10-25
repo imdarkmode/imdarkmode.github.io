96388bc0dd7b9c59a522ddda7eb58970e9a41cac
Dark Mode <darkkmodee00@gmail.com>
2023-10-24 20:00:27 -0400

    feature: better text for phones

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue'
+const windowWidth = ref(window.innerWidth)
+const windowHeight = ref(window.innerHeight)
+
+const handleResize = () => {
+  windowWidth.value = window.innerWidth
+  windowHeight.value = window.innerHeight
+  console.log("height: ", windowHeight.value)
+  console.log("width:", windowWidth.value)
+}
+
+onMounted(() => {
+  window.addEventListener('resize', handleResize)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', handleResize)
+})
+
+</script>
 
 <template>
   <main>
     <div class="full-center-container">
-      <div style="width: 100%">
-        <img src="/src/assets/pictures/panther_home.png" style="width: 100%">
+      <div style="width: 100%; height: 100%;">
+        <img src="/src/assets/pictures/panther_home.png" class="home-image">
       </div>
     </div>
   </main>
