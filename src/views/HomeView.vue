c0b9c3267fbd5ba431bbc03c9a0b369e35344a67
Dark Mode <darkkmodee00@gmail.com>
2023-10-25 11:40:43 -0400

    feature: home picture resizing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,33 +1,10 @@
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted } from 'vue'
-const windowWidth = ref(window.innerWidth)
-const windowHeight = ref(window.innerHeight)
-
-const handleResize = () => {
-  windowWidth.value = window.innerWidth
-  windowHeight.value = window.innerHeight
-  console.log("height: ", windowHeight.value)
-  console.log("width:", windowWidth.value)
-}
-
-onMounted(() => {
-  window.addEventListener('resize', handleResize)
-})
-
-onUnmounted(() => {
-  window.removeEventListener('resize', handleResize)
-})
-
 </script>
 
 <template>
-  <main>
-    <div class="full-center-container">
-      <div style="width: 100%; height: 100%;">
-        <img src="/src/assets/pictures/panther_home.png" class="home-image">
-      </div>
-    </div>
-  </main>
+  <v-app>
+    <v-img cover src="/src/assets/pictures/panther_2560x1440.png"></v-img>
+  </v-app>
 </template>
 
 <style scoped lang="sass"></style>
