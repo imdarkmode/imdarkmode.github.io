1521c0d0af48047cfdf0a57d6ca89b9d4374b530
Dark Mode <darkkmodee00@gmail.com>
2023-10-25 13:54:11 -0400

    bug: idk changed path

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@ import FeaturedProjects from "@/components/FeaturedProjects.vue"
 
 <template>
   <v-app>
-    <v-img cover src="/src/assets/pictures/panther_home.png"></v-img>
+    <v-img cover src="/src/assets/panther.png"></v-img>
     <FeaturedProjects></FeaturedProjects>
   </v-app>
 </template>
