bcbd8e3c840e2d2468d7e8e0b83e15d737333d5c
Dark Mode <darkkmodee00@gmail.com>
2023-10-24 22:46:59 -0400

    feature: hamburg selector

--- src/components/headnav.vue
+++ src/components/headnav.vue
@@ -1,12 +1,42 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const drawer = ref(false)
+const items = ref([
+  {
+    title: 'Projects',
+    value: 'projects',
+  },
+  {
+    title: 'Store',
+    value: 'store',
+  },
+  {
+    title: 'About',
+    value: 'about',
+  },
+  {
+    title: 'Contact',
+    value: 'contact',
+  },
+])
+</script>
 
 <template>
-  <main>
-    <nav class="headnav">
-      <RouterLink class="headnav-link" to="/">Home</RouterLink>
-      <RouterLink class="headnav-link" to="/projects">Projects</RouterLink>
-      <RouterLink class="headnav-link" to="/projects">Store</RouterLink>
-      <RouterLink class="headnav-link" to="/about">About</RouterLink>
-    </nav>
-  </main>
+  <v-app>
+    <v-container>
+      <v-app-bar :elevation="8">
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-app-bar-title>Dark Mode</v-app-bar-title>
+        <v-spacer></v-spacer>
+        <v-btn variant="text" icon="mdi-magnify"></v-btn>
+        <v-btn variant="text" icon="mdi-filter"></v-btn>
+        <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+      </v-app-bar>
+    </v-container>
+
+    <v-navigation-drawer v-model="drawer" location="left" temporary>
+      <v-list :items="items"></v-list>
+    </v-navigation-drawer>
+  </v-app>
 </template>
