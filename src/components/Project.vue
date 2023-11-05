02b91dc757af374e7b972d968ebde0b76f116cbd
Dark Mode <darkkmodee00@gmail.com>
2023-11-05 15:39:52 -0500

    feature: projects frontend

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,24 +1,47 @@
 <script setup lang="ts">
 import { defineProps } from 'vue'
 
-// TODO: finish this component
-
 defineProps({
-  url: String,
-  header: String,
-  text: String
+  image: {
+    type: String,
+    required: false
+  },
+  youtubeUrl: {
+    type: String,
+    required: false
+  },
+  githubUrl: {
+    type: String,
+    required: false
+  },
+  websiteUrl: {
+    type: String,
+    required: false
+  },
+  header: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
 })
 
 </script>
 
 <template>
   <v-container class="container">
-    <iframe class="youtube" :src="url"></iframe>
-    <div class="text-container">
-      <h2>{{ header }}</h2>
-      <p>{{ text }}</p>
-    </div>
-
+    <iframe class="youtube" :src="youtubeUrl"></iframe>
+    <v-container class="text-container">
+      <h2 class="header">{{ header }}</h2>
+      <p class="description">{{ description }}</p>
+    </v-container>
+    <v-container class="icon-container">
+      <a target="_blank" v-if="githubUrl" :href="githubUrl"><v-icon>mdi-github</v-icon></a>
+      <a target="_blank" v-if="websiteUrl" :href="websiteUrl"><v-icon>mdi-web</v-icon></a>
+      <a target="_blank" v-if="youtubeUrl" :href="youtubeUrl"><v-icon>mdi-youtube</v-icon></a>
+    </v-container>
   </v-container>
 </template>
 
@@ -31,12 +54,35 @@ defineProps({
   display: flex
   flex-direction: row
   
+.image-container
+  display: flex
+  flex-direction: column
+  justify-content: center
+  margin-left: 10px
+  margin-right: 10px
+  flex: 1
+
 .text-container
   display: flex
   flex-direction: column
   justify-content: center
-  margin-left: 20px
-  margin-right: 20px
+  margin-left: 10px
+  margin-right: 10px
   flex: 3
 
+.icon-container
+  display: flex
+  flex-direction: column
+  justify-content: center
+  margin-left: 10px
+  margin-right: 10px
+  flex: 0
+
+.description
+  font-size: 13px
+
+.header
+  font-size: 19px
+
 </style>
+
