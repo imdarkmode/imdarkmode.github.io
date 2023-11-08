31d2d3250dd922b62b4c70a9a36fbd0a972e7c63
Dark Mode <darkkmodee00@gmail.com>
2023-11-08 14:47:43 -0500

    bug: updated website depths urls

--- src/data.ts
+++ src/data.ts
@@ -5,8 +5,8 @@ let projects: IProject[] = [
     header: "Website Depths",
     description: "Journey with me down to the deepest depths of the web. To do this, I developed a website capable of showing measurements of various large objects, to test if they would fit inside the website, as well as markers at every foot. The end goal was to get to the same depth as the bottom of the deepest part of the ocean, the Mariana Trench.",
     youtubeUrl: "https://www.youtube.com/embed/ZXlAVOFNTw4",
-    githubUrl: "https://github.com/thedarkmode/InspectGame",
-    websiteUrl: "https://thedarkmode.github.io/InspectGame/"
+    githubUrl: "https://github.com/thedarkmode/WebsiteDepths",
+    websiteUrl: "https://thedarkmode.github.io/WebsiteDepths"
   }
 ]
 
