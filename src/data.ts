e5bee425d7b8dcd7067765ee69aa34cc1781c25b
Dark Mode <darkkmodee00@gmail.com>
2023-11-08 22:56:22 -0500

    added website depths link

--- src/data.ts
+++ src/data.ts
@@ -4,7 +4,7 @@ let projects: IProject[] = [
   {
     header: "Website Depths",
     description: "Journey with me down to the deepest depths of the web. To do this, I developed a website capable of showing measurements of various large objects, to test if they would fit inside the website, as well as markers at every foot. The end goal was to get to the same depth as the bottom of the deepest part of the ocean, the Mariana Trench.",
-    youtubeUrl: "https://www.youtube.com/embed/ZXlAVOFNTw4",
+    youtubeUrl: "https://youtu.be/kPxkH1_Rgnw",
     githubUrl: "https://github.com/thedarkmode/WebsiteDepths",
     websiteUrl: "https://thedarkmode.github.io/WebsiteDepths"
   }
