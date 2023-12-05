17f46ff1eb5964fa93a848d82937bf1e2143387f
Dark Mode <darkkmodee00@gmail.com>
2023-12-04 19:09:04 -0500

    feature: tik tok link home screen

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -28,6 +28,14 @@ function navigateTo(url: string) {
         <v-row justify="center">
           <v-btn class="link-btn" color="primary"
             @click="navigateTo('https://www.youtube.com/channel/UC-qdwsjhmZ6eS8rlk2lg1Jw')"><v-icon>mdi-youtube</v-icon>Youtube</v-btn>
+          <v-btn class="link-btn" @click="navigateTo('https://www.tiktok.com/@imdarkmode')"><v-icon>
+              <svg style="padding-right: 2px" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                fill="currentColor" class="bi bi-tiktok" viewBox="0 0 16 16">
+                <path
+                  d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z" />
+              </svg>
+            </v-icon>Tik
+            Tok</v-btn>
           <v-btn class="link-btn"
             @click="navigateTo('https://github.com/thedarkmode?tab=repositories')"><v-icon>mdi-github</v-icon>Github</v-btn>
           <v-btn class="link-btn" @click="navigateTo('https://discord.gg/GmPZ3e7tZH')"><svg
