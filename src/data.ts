ada18dc24e6a95d57b7dd163f5f205a8e1d600a8
Dark Mode <darkkmodee00@gmail.com>
2023-12-08 13:54:54 -0500

    feature: added parallax project

--- src/data.ts
+++ src/data.ts
@@ -2,6 +2,19 @@ import type { IProject } from "@/interfaces";
 
 let projects: IProject[] = [
   {
+    header: "Parallax Windows",
+    description: "Learn how to effortlessly create a captivating parallax effect for your website! Developers of any level can try this out themselves!",
+    youtubeUrl: "https://www.youtube.com/embed/QB0VbW65uZM?si=vzoGpEk1TYjV8eef",
+    githubUrl: "https://github.com/thedarkmode/parallaxwindows",
+    websiteUrl: "https://thedarkmode.github.io/parallaxwindows",
+    createdWith: [
+      {
+        name: "Vue.js",
+        icon: ""
+      }
+    ]
+  },
+  {
     header: "Website Depths",
     description: "Journey with me down to the deepest depths of the web. To do this, I developed a website capable of showing measurements of various large objects, to test if they would fit inside the website, as well as markers at every foot. The end goal was to get to the same depth as the bottom of the deepest part of the ocean, the Mariana Trench.",
     youtubeUrl: "https://www.youtube.com/embed/kPxkH1_Rgnw",
@@ -13,7 +26,7 @@ let projects: IProject[] = [
         icon: ""
       }
     ]
-  }
+  },
 ]
 
 export {
