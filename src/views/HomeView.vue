c4b4be3621051ca051cef240c269a22659c3deac
Dark Mode <darkkmodee00@gmail.com>
2023-12-14 19:40:24 -0500

    tiktok one word

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -34,8 +34,7 @@ function navigateTo(url: string) {
                 <path
                   d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z" />
               </svg>
-            </v-icon>Tik
-            Tok</v-btn>
+            </v-icon>TikTok</v-btn>
           <v-btn class="link-btn"
             @click="navigateTo('https://github.com/thedarkmode?tab=repositories')"><v-icon>mdi-github</v-icon>Github</v-btn>
           <v-btn class="link-btn" @click="navigateTo('https://discord.gg/GmPZ3e7tZH')"><svg
